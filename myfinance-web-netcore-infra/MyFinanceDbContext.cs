5fa1f34a345813312c902377413b4a0e03b298e6
lfcorrea94 <lfcorrea94@gmail.com>
2025-07-23 19:05:46 -0300

    Add entities and dbContext

---
+++ myfinance-web-netcore-infra/MyFinanceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myfinance_web_netcore_domain.Entities;
+
+namespace myfinance_web_netcore_infra;
+
+public class MyFinanceDbContext : DbContext
+{
+    public DbSet<PlanoConta> DbSetPlanoConta { get; set; }
+    public DbSet<Transacao> DbSetTransacao { get; set; }    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=.\SQLEXPRESSD;Database=myFinance;Trusted_Connection=True;");
+    }
+}
